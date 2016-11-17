c369ecd8ced6ccaf1625e7b3f03970dc75104a6b
Angel1912 <ingenieromagel2@gmail.com>
2016-11-17 14:06:59 -0600

    Puli el funcionamiento de el registro
    Añadi unas cuantas validaciones a las entradas al JSON

--- Gallos/js/gallo.js
+++ Gallos/js/gallo.js
@@ -1,6 +1,20 @@
 app.factory('gallo',function(){
-	var gallos=[{numero:'1',partido:'Juanito'},{numero:'2',partido:'Pablito'},{numero:3,partido:'Juanito'},{numero:4,partido:'Juanito'},{numero:5,partido:'Juanito'},{numero:6,partido:'Cesarini'}]
+	//var gallos=[{numero:'1',partido:'Juanito',peso:'110'},{numero:'2',partido:'Pablito',peso:'120'},{numero:3,partido:'Juanito',peso:'90'},{numero:4,partido:'Juanito',peso:'160'},{numero:5,partido:'Juanito',peso:'150'},{numero:6,partido:'Cesarini',peso:'200'}]
+	var gallos=[]
 	var obj={
+		addGallo:function(partido,numero,peso){
+			if (partido!=''&&numero!=''&&peso!=''&& partido && numero && peso) {
+				var valida=alasql('SELECT * FROM ? WHERE numero="'+numero+'"', [gallos])
+				if(valida.length==0){
+					gallos.push({'numero':numero,'partido':partido,'peso':peso})
+					return 0;
+				}else{
+					return 2;
+				}
+			} else{
+				return 1;
+			};
+		},
 		getGallo:function(numero){
 			if(numero!=''){
 				return alasql('SELECT * FROM ? WHERE numero="'+numero+'"', [gallos]);
--- Gallos/js/registro/regCtrl.js
+++ Gallos/js/registro/regCtrl.js
@@ -7,4 +7,11 @@ app.controller('regCtrl',['$scope','gallo',function($scope,gallo){
 	s.getGallosPartido=function(){
 		s.gallos=gallo.getGallosPartido(s.partido)
 	}
+	s.addGallo=function(){
+		s.validaRegistro=gallo.addGallo(s.partido,s.numero,s.peso)
+		if(s.validaRegistro==0){
+			s.gallos=gallo.getGallosPartido(s.partido);
+			s.partidos=gallo.getAllPartido();
+		}
+	}
 }])
